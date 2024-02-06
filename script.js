df55a81f2d4655ac6f71ceb7bc10a787e7a321cd
Gichana565 <gichanaryan565@gmail.com>
2024-02-06 14:23:05 +0300

    Added script js file to demonstrate the basics of JavaScript.

---
+++ script.js
@@ -0,0 +1,29 @@
+//Printing Hello world in the console
+//console.log("Hello World!");
+
+//Displaying an alert to a user in an HTML page
+alert("Hello World!")
+
+//Alert, prompt and confirm
+//Alert allows a user to display a message on the dialog box
+alert("This is an alert box")
+
+//prompt asks a visitor to input information and stores it in a variable
+var visitorName = prompt("What's your name : ")
+console.log(visitorName)
+
+//confirm displays a dialog box with 2 buttons, OK returns true while cancel returns false
+var isCoolWithIt = confirm("Are you okay with it?")
+console.log(isCoolWithIt)
+
+//JS Comments
+//Single line comment
+var a = 2 + 2 // I am a single line comment
+
+/*
+This is a multi-lines comment which will get ignored by the 
+Javascript interpreter
+var b = 3 + 3
+*/
+
+
